a173d7c065dc3d05cf5bd61e54f13325acfbad08
varMarat <56462327+varMarat@users.noreply.github.com>
2020-08-25 21:35:02 +0600

    first commit

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="my-form-group">
+        <label for="">Password</label>
+        <input type="password" class="my-form-control">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'FormGroup'
+}
+</script>
+
+<style lang="scss">
+.my-form-control{
+    width: 100%;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
+    margin: 8px 0;
+    outline: none;
+    padding: 6px;
+    box-sizing: border-box;
+    transition: 0.3s;
+    &:focus{
+        border-color: rgb(30, 144, 255);
+        box-shadow: 0 0 8px 0 rgb(30, 144, 255);
+    }
+}
+</style>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <form action="">
+        <FormGroup/>
+        <FormGroup/>
+        <FormGroup/>
+    </form>
+  </div>
+</template>
+
+<script>
+import FormGroup from './FormGroup.vue'
+
+export default {
+  name: 'myForm',
+  components:{
+      FormGroup
+  }
+}
+</script>
+
+<style lang="scss">
+  .wrapper{
+    width: 50%;
+    margin: 0 auto;
+    border: 2px solid rgb(121, 55, 207);
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
